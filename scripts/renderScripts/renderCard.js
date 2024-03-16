f58a98bc444265ded23057699f3249234273d99b
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-03-16 03:53:28 -0700

    change renderCard to use vanilla js

--- scripts/renderScripts/renderCard.js
+++ scripts/renderScripts/renderCard.js
@@ -1,17 +1,20 @@
 // CARD_CONTENT FORMAT: projectName, projectGenre, projectImageStatic, projectImageGif, projectDescription
 
 async function renderCard(cardContent, container) {
-    // load html template into variable
-    $.get('./templates/card.html', (rawContent) => {
-        // format string response into JS object
-        let content = $('<div />', {html:rawContent});
+    // fetch template html
+    let rawTemplate = await loadHTML('../../templates/card.html');
 
-        // modify template to show passed in content
-        $(content).find('.cardTemplateTitle').html(cardContent[0]);
-        $(content).find('.cardTemplateSubtitle').html(cardContent[1]);
-        $(content).find('.cardTemplateDescription').html(cardContent[4]);
-        $(content).find('.cardTemplateStaticImage').attr("src", cardContent[2])
-        $(content).find('.cardTemplateDynamicImage').attr("src", cardContent[3]);
-        container.innerHTML += content.html();
-    });
+    // parse template string to DOM structure
+    let parser = new DOMParser();
+    let template = parser.parseFromString(rawTemplate, 'text/html').body;
+    
+    // input parameters
+    template.querySelector('.cardTemplateTitle').innerHTML = cardContent[0];
+    template.querySelector('.cardTemplateSubtitle').innerHTML = cardContent[1];
+    template.querySelector('.cardTemplateDescription').innerHTML = cardContent[4];
+    template.querySelector('.cardTemplateStaticImage').src = cardContent[2];
+    template.querySelector('.cardTemplateDynamicImage').src = cardContent[3];
+
+    // append to container
+    container.innerHTML += template.outerHTML;
 }
