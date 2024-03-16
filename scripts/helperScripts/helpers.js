56ac6fde504c1a89ace7c8087bfcea08c5587d94
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-03-16 07:44:17 -0700

    add helper functions for css effects

--- scripts/helperScripts/helpers.js
+++ scripts/helperScripts/helpers.js
@@ -2,3 +2,65 @@ async function loadHTML(filepath) {
     content = await fetch(filepath);
     return content.text();
 } 
+
+function fadeOut(element, endState, spd) {
+    if (endState != "hidden" && endState != "none") {
+        return;
+    }
+    element.style.opacity = 1;
+    (function fade() {
+        let opacityVal = parseFloat(element.style.opacity);
+        if ((opacityVal -= spd) > 0) {
+            element.style.opacity = opacityVal;
+            requestAnimationFrame(fade);
+        } else {
+            if (endState === "hidden") {
+                element.style.visibility = endState;
+            } else if (endState === "none") {
+                element.style.display = endState;
+            }
+        }
+    })();
+}
+
+function fadeIn(element, endState, spd) {
+    element.style.opacity = 0;
+    if (endState === "visible") {
+        element.style.visibility = endState;
+    } else if (endState === "block") {
+        element.style.display = endState;
+    } else {
+        return
+    }
+
+    (function fade() {
+        let opacityVal = parseFloat(element.style.opacity);
+        if ((opacityVal += spd) < 1) {
+            element.style.opacity = opacityVal;
+            requestAnimationFrame(fade);
+        }
+    })();
+}
+
+async function getImageDimensions(filepath) {
+    return new Promise((resolve) => {
+        let img = new Image();
+
+        img.onload = () => {
+            resolve({width: img.width, height: img.height});
+        }
+        img.onerror = () => {
+            resolve(null);
+        }
+        img.src = filepath;
+    })
+}
+
+function addHoverEffect(parentNode, childNode, enterEffect, leaveEffect) {
+    parentNode.addEventListener('mouseenter', () => {
+        enterEffect(childNode);
+    });
+    parentNode.addEventListener('mouseleave', () => {
+        leaveEffect(childNode);
+    });
+}
