9adf6bb279a91eb65d62129c0ca7c05f9a79d1fe
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-19 14:51:15 -0700

    use cardObject for populating cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 // FORMAT: projectName, projectGenre, projectImageStatic, projectImageGif, projectDescription
 
+// card content for each project
 const swingProjectContent = ['Swing', 'Endless Runner Arcade Game', './images/swingProject.PNG', './images/swingProject.gif', 
 'Swing is an early project I made as the capstone project to the CS50x online course. It is an endless runner game inspired by the late Flappy Bird, where you must use your grapple hook to swing through the gaps and accumulate as many points as possible. I developed this game using the Pygame library, where I implemented all game logic and created all art assets from scratch.'];
 
@@ -12,7 +13,7 @@ const lapsProjectContent = ['LAPS', 'Live Bus Tracking Webapp', './images/lapsPr
 const discordBotProjectContent = ['Schedule Bot (In Development)', 'Discord Schedule Plan Bot', './images/discordBotProject.png', '',
 'Schedule Bot is a Discord bot I created to help organize due dates at school. The bot aims to consolidate features useful for students into one place. Features include due date reminders, notes saving, checklist planners, etc.'];
 
-const pawsitiveHackathonProjectContent = [ 'PAWsitive', 'Mobile Webapp Game', './images/PAWsitiveHomePage.png', 'TODO',
+const pawsitiveHackathonProjectContent = ['PAWsitive', 'Mobile Webapp Game', './images/PAWsitiveHomePage.png', 'TODO',
 'PAWsitive is a project submission for QDS Hackathon 2024, it was designed to help promote the development of healthy routines within players by gamifying such behaviors. The game presents dailies for players, in which players do randomly generated healthy activities to get in-game currency. This currency can then be spent within either an in-game shop, or players can choose to use the currency for gacha.'
 ]
 
@@ -29,15 +30,23 @@ document.addEventListener('DOMContentLoaded', async () => {
     document.body.style.visibility = 'hidden';
     const cardContainer = document.getElementById('container');
 
-    // render the cards
-    await renderCard(swingProjectContent, cardContainer);
-    await renderCard(pawsitiveHackathonProjectContent, cardContainer);
-    await renderCard(perfectDarknessProjectContent, cardContainer);
-    await renderCard(lapsProjectContent, cardContainer);
-    await renderCard(discordBotProjectContent, cardContainer);
+    // create objects for each card
+    let swingProject = new card(swingProjectContent);
+    let perfectDarknessProject = new card(perfectDarknessProjectContent);
+    let lapsProject = new card(lapsProjectContent);
+    let discordBotProject = new card(discordBotProjectContent);
+    let pawsitiveHackathonProject = new card(pawsitiveHackathonProjectContent);
 
-    // set each card description to hidden for hover effect
+    // render the cards using card objects
+    await renderCard(swingProject, cardContainer);
+    await renderCard(pawsitiveHackathonProject, cardContainer);
+    await renderCard(perfectDarknessProject, cardContainer);
+    await renderCard(lapsProject, cardContainer);
+    await renderCard(discordBotProject, cardContainer);
+
+    // set hover effects for every card
     let allCardDescription = document.querySelectorAll('.cardTemplateDescription');
+    let allCardBackgroundImages = document.querySelectorAll('.cardTemplateStaticImage');
     allCardDescription.forEach((card) => {
         card.style.display = 'none';
 
