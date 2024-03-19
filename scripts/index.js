1fdd98f6f824c22bfad9bddc007a1a477a890dc9
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-19 14:18:59 -0700

    add images of projects and linked it into page

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,13 @@
 const swingProjectContent = ['Swing', 'Endless Runner Arcade Game', './images/swingProject.PNG', './images/swingProject.gif', 
 'Swing is an early project I made as the capstone project to the CS50x online course. It is an endless runner game inspired by the late Flappy Bird, where you must use your grapple hook to swing through the gaps and accumulate as many points as possible. I developed this game using the Pygame library, where I implemented all game logic and created all art assets from scratch.'];
 
-const perfectDarknessProjectContent = ['Perfect Darkness', 'Turn-based Horror Game', 'TODO', 'TODO', 
+const perfectDarknessProjectContent = ['Perfect Darkness', 'Turn-based Horror Game', './images/perfectDarkness.png', '', 
 'Perfect Darkness (Terrible name I know...) is a horror game I made as the final project for one of my courses at BCIT. It is turned-based game where you are tasked with eliminating a monster that is significantly stronger than you. You must scavage for loot in pure darkness in order to get strong enough to kill the monster, all while the creature is roaming about in the building you are in.'];
 
-const lapsProjectContent = ['LAPS', 'Live Bus Tracking Webapp', 'TODO', 'TODO',
+const lapsProjectContent = ['LAPS', 'Live Bus Tracking Webapp', './images/lapsProject.jpg', '',
 'LAPS is a webapp that provides real-time bus locations using TransLink\'s API. It is aimed at providing transit goers agency in their decision making process.'];
 
-const discordBotProjectContent = ['Schedule Bot (In Development)', 'Discord Schedule Plan Bot', 'TODO', 'TODO',
+const discordBotProjectContent = ['Schedule Bot (In Development)', 'Discord Schedule Plan Bot', './images/discordBotProject.png', '',
 'Schedule Bot is a Discord bot I created to help organize due dates at school. The bot aims to consolidate features useful for students into one place. Features include due date reminders, notes saving, checklist planners, etc.'];
 
 const pawsitiveHackathonProjectContent = [ 'PAWsitive', 'Mobile Webapp Game', './images/PAWsitiveHomePage.png', 'TODO',
