33dd23f6fc173df7dd62fce68b59c27176cf5aa8
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-19 16:28:54 -0700

    add hover effect on background

--- scripts/helperScripts/hoverEffects.js
+++ scripts/helperScripts/hoverEffects.js
@@ -1,7 +1,31 @@
+// SECTION: Hover Effects
+
 function showDescription(element) {
-    fadeIn(element, "block", 0.1);
+    fadeIn(element, "block", 0.05);
 }
 
 function hideDescription(element) {
-    fadeOut(element, "none", 0.1);
+    fadeOut(element, "none", 0.05);
+}
+
+function showGif(element, cardObject) {
+    console.log(cardObject);
+    if (cardObject.projectImageGif !== null) {
+        element.style.backgroundImage = `url(${cardObject.projectImageGif})`;
+    }
+}
+
+function showStatic(element, cardObject) {
+    element.style.backgroundImage = `url(${cardObject.projectImageStatic})`;
+}
+
+// SECTION: Hover Effect Helpers
+
+function addHoverEffect(parentNode, childNode, cardObject, enterEffect, leaveEffect) {
+    parentNode.addEventListener('mouseenter', () => {
+        enterEffect(childNode, cardObject);
+    });
+    parentNode.addEventListener('mouseleave', () => {
+        leaveEffect(childNode, cardObject);
+    });
 }
