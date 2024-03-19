ca6365181259ad593d6f292991a248c0e185ea44
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-19 14:49:31 -0700

    create object for cards

---
+++ scripts/objects/cardObject.js
@@ -0,0 +1,20 @@
+class card {
+
+    projectDescription;
+    projectGenre;
+    projectImageGif;
+    projectImageStatic;
+    projectName;
+
+    constructor (projectDataArray) {
+        this.projectName = projectDataArray[0];
+        this.projectGenre = projectDataArray[1];
+        this.projectImageStatic = projectDataArray[2];
+        this.projectDescription = projectDataArray[4];
+
+        // if there is no gif, set it to null
+        if (projectDataArray[3] == '') {
+            this.projectImageGif = null;
+        }
+    }
+}
